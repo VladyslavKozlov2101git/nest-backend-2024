6507f48c8d947df79c0e2859509fe1bc376e1eac
VladyslavKozlov <vladislavkozlov2101@gmail.com>
2024-09-25 16:50:19 +0300

    Auth Serivce 1:22:17

---
+++ src/auth/decorators/user.decorator.ts
@@ -0,0 +1,9 @@
+import type { User } from "@prisma/client";
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+export const CurrentUser = createParamDecorator(
+  (data: keyof User, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return data ? user?.[data] : user;
+  })
