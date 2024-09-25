d89e82ee91d1092efee2b991376440a8b5bb38c2
VladyslavKozlov <vladislavkozlov2101@gmail.com>
2024-09-25 20:04:20 +0300

    added cookies params 1:38:33

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,7 +9,7 @@ import { Response } from 'express';
 export class AuthService {
     EXPIRE_DAY_REFRESH_TOKEN = 1
     REFRESH_TOKEN_NAME = "refreshToken"
-    
+
     constructor(
         private jwt: JwtService,
         private userService: UserService,
@@ -71,6 +71,18 @@ export class AuthService {
             secure:true,
             sameSite:"none"
 
+        })
+
+    }
+
+    
+    removeRefreshTokenToResponse(res:Response, ){
+        res.cookie(this.REFRESH_TOKEN_NAME, "", {
+            httpOnly:true,
+            domain:"localhost",
+            expires: new Date(0),
+            secure:true,
+            sameSite:"none"
         })
 
     }
