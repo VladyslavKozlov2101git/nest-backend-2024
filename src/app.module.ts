073a2cbce1935ab6a6232dee25712b0876a2cc65
VladyslavKozlov <vladislavkozlov2101@gmail.com>
2024-09-30 16:17:59 +0300

    Added timer 2:30

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 import { TaskModule } from './task/task.module'
+import { PomodoroModule } from './pomodoro/pomodoro.module'
 import { TimeBlockModule } from './time-block/time-block.module'
 import { ConfigModule } from '@nestjs/config'
 
@@ -12,7 +13,8 @@ import { ConfigModule } from '@nestjs/config'
 		AuthModule,
 		UserModule,
 		TaskModule,
-		TimeBlockModule
+		TimeBlockModule,
+		PomodoroModule
 	]
 })
 export class AppModule {}
